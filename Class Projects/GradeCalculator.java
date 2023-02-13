bdd2760b9f2aabb2d3a92f9de3e7a9fe82b65448
Z4-G <55454111+Z4-G@users.noreply.github.com>
2023-02-12 22:03:45 -0700

    Grade calc committed

---
+++ Class Projects/GradeCalculator.java
@@ -0,0 +1,41 @@
+import java.util.InputMismatchException;
+import java.util.Scanner; 
+public class GradeCalculator {
+    public static void main(String[] args){
+        int grade = 0; 
+
+        Scanner gradeValue = new Scanner(System.in);
+        System.out.println("Please enter your grade as an interger.");
+        grade = gradeValue.nextInt();
+
+        while (grade < 0){
+            System.out.println("please enter a value greater than -1");
+            grade = gradeValue.nextInt();
+        }
+        
+
+        if (grade >= 0 && grade < 60) {
+            System.out.println("Your grade is F");
+        } else if (grade <= 60 && grade < 67) {
+            System.out.println("Your grade is D+");
+        } else if (grade >= 67 && grade < 69) {
+            System.out.println("Your grade is D");
+        } else if (grade >= 69 && grade < 72 ) {
+            System.out.println("Your grade is C-");
+        } else if (grade >= 72 && grade < 77) {
+            System.out.println("Your grade is C");
+        } else if (grade >= 77 && grade < 79) {
+            System.out.println("Your grade is C+");
+        } else if (grade >= 79 && grade < 82) {
+            System.out.println("Your grade is B-");
+        } else if (grade >= 82 && grade < 87) {
+            System.out.println("Your grade is B");
+        } else if (grade >= 87 && grade < 89) {
+            System.out.println("Your grade is B+");
+        } else if (grade >= 89 && grade < 92) {
+            System.out.println("Your grade is A-");
+        } else if (grade >= 92) {
+            System.out.println("Your grade is A");
+        }
+    }
+}
--- Class Projects/README.md
+++ Class Projects/README.md
@@ -1,2 +1,3 @@
+
 # Temp_Convert
 ** Takes a temperature unit (Celcius, Kelvin, Or Farenheit) and converts between each other. **
