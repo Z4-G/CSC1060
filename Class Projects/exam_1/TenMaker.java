5475fda3f8530432dc9ec8064d6998e48c7fe8d7
Z4-G <55454111+Z4-G@users.noreply.github.com>
2023-02-26 16:24:03 -0700

    Semantics

--- Class Projects/exam_1/TenMaker.java
+++ Class Projects/exam_1/TenMaker.java
@@ -1,19 +1,20 @@
 package exam_1;
 
 public class TenMaker{
+    static boolean check = true;
     public static void makes10(int unit1, int unit2){
-    boolean check = true;
      if (unit1 + unit2 == 10){
          check = true;
-         System.out.println("The sum of these values is 10");
-         System.out.println("check = " + check);
      } else {
          check = false;
-         System.out.println("This sum of these values is not 10");
-         System.out.println("check = " + check);
      }
- }
- public static void main(String[] args){
-    makes10(4,5);
- }
+    }
+    public static void main(String[] args){
+        makes10(5,5);
+        if (check == true){
+            System.out.println("The sum of these values is ten.");
+        }else if (check == false){
+            System.out.println("The sum of these values is not ten.");
+        }
+    }
 }
