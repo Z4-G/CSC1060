89e761aab965386df4d8117718febc36d3edc9db
Z4-G <55454111+Z4-G@users.noreply.github.com>
2023-02-26 16:40:49 -0700

    It does the thing

--- Class Projects/BottlesOfBeer.java
+++ Class Projects/BottlesOfBeer.java
@@ -11,20 +11,25 @@ public class BottlesOfBeer {
         final int save = bottles;
         
         for ( int i = bottles; i > 0; i -= 1){
-            
+
             if (bottles > 1){
-            System.out.println(bottles + " bottles of beer on the wall. \n" + bottles + " bottles of beer.");
-            bottles  -= 1;
-            System.out.println("Take one down, pass it around " + bottles + " bottles of beer on the wall.");
+                System.out.println(bottles + " bottles of beer on the wall. \n" + bottles + " bottles of beer.");
+                bottles  -= 1;
+                if (bottles >= 2){
+                    System.out.println("Take one down, pass it around " + bottles + " bottles of beer on the wall.");
+                } else if (bottles < 2){
+                    System.out.println("Take one down, pass it around " + bottles + " bottle of beer on the wall.");
+                }
             } else if (bottles == 1){
-            System.out.println(bottles + " bottle of beer on the wall. \n" + bottles + " bottle of beer.");
-            bottles  -= 1;
-            System.out.println("Take one down, pass it around " + bottles + " bottles of beer on the wall.");
+                System.out.println(bottles + " bottle of beer on the wall. \n" + bottles + " bottle of beer.");
+                bottles  -= 1;
+                System.out.println("Take one down, pass it around " + bottles + " bottles of beer on the wall.");
             }
-            if (save > 1){
-            System.out.println("Go to the store, buy some more " + save + " bottles of beer on the wall.");
-            } else if (save == 1 ){
-            System.out.println("Go to the store, buy some more " + save + " bottle of beer on the wall.");  
+
+            if (save > 1 && bottles == 0){
+                System.out.println("Go to the store, buy some more " + save + " bottles of beer on the wall.");
+            } else if (save == 1 && bottles == 0){
+                System.out.println("Go to the store, buy some more " + save + " bottle of beer on the wall.");  
             }
             input.close();
         }
